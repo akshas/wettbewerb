f4aedf84e669339784cc6ea3d14d851a66b0e617
Alex <allexxx81@gmail.com>
2020-08-19 15:54:09 +0200

    wpf wettbewerb

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfWettbewerb
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            GameStarten();
+        }
+
+        private void GameStarten()
+        {
+            if(Player1.Text != "" && Player2.Text != "")
+            {
+                Spieler sp1 = new Spieler(Player1.Text);
+                Spieler sp2 = new Spieler(Player2.Text);
+
+                NamePlayer1.Text = sp1.Name + "s Leben: " + sp1.Leben;
+                NamePlayer2.Text = sp2.Name + "s Leben: " + sp2.Leben;
+            }
+        }
+    }
+}
---
+++ Spieler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfWettbewerb
+{
+    class Spieler
+    {
+        public string Name { get; set; }
+        public int Leben { get; set; }
+        public int Schlag { get; set; }
+
+        Random rnd = new Random();
+        public Spieler(string name)
+        {
+            Name = name;
+            Leben = 0;
+            Schlag = 0;
+        }
+
+        public void Hauen()
+        {
+            
+        }
+    }
+}
